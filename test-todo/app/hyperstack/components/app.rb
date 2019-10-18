59034659a5d2aa1b210f706e549652b555b988b1
sebi <sebyx07.pro@gmail.com>
2019-10-18 10:42:28 +0100

    change

---
+++ test-todo/app/hyperstack/components/app.rb
@@ -0,0 +1,18 @@
+class App < HyperComponent
+  include Hyperstack::Router
+  render do
+    DIV do
+
+      # define routes using the Route psuedo component.  Examples:
+      # Route('/foo', mounts: Foo)                : match the path beginning with /foo and mount component Foo here
+      # Route('/foo') { Foo(...) }                : display the contents of the block
+      # Route('/', exact: true, mounts: Home)     : match the exact path / and mount the Home component
+      # Route('/user/:id/name', mounts: UserName) : path segments beginning with a colon will be captured in the match param
+      # see the hyper-router gem documentation for more details
+
+      H1 { "Hello world from Hyperstack!" }
+      H1 { "Number of Todos: #{Todo.count}" }
+    end
+  end
+end
+
