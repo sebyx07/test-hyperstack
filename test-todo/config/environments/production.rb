7e9025b34ab432d542293d064890f2e922f2773a
sebi <sebyx07.pro@gmail.com>
2019-10-18 11:01:23 +0100

    change

--- test-todo/config/environments/production.rb
+++ test-todo/config/environments/production.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = ['http://rr8g.hatchboxapp.com']
+  config.web_socket_server_url = "ws://rr8g.hatchboxapp.com/"
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
