4c1724b7fa62753fb99d1a1ad840ba4a3d4238e1
sebi <sebyx07.pro@gmail.com>
2019-10-18 11:17:40 +0100

    update

--- test-todo/app/assets/javascripts/application.js
+++ test-todo/app/assets/javascripts/application.js
@@ -9,6 +9,5 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery
 //= require hyperstack-loader
 //= require_tree .
